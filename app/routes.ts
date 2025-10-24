3d999a2a7393774840804d9bc6bbdefecfdc27b8
Seno Aji <cydnirn.dev@gmail.com>
2025-10-25 00:10:21 +0700

    fix: Fix image to use id

--- app/routes.ts
+++ app/routes.ts
@@ -3,5 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("concert/:id", "routes/concert.$id.tsx"),
-  route("images/:id.jpeg", "routes/images.$id[.]jpeg.tsx"),
+  route("images/:id", "routes/images.$id.tsx"),
 ] satisfies RouteConfig;
